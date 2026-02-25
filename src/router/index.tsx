a19961268ec0e6d97a4c0d4ef2fa937e7a41ea6c
Rohit <sinharohit96690@gmail.com>
2026-02-25 10:53:46 +0530

    feat(auth): add login page and routing setup

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import LoginPage from '../features/login/pages';
 
 const router = createBrowserRouter([
   {
+    path: '/',
+    element: <Navigate to="/login" replace />,
+  },
+  {
     path: '/login',
     element: <LoginPage />,
+  },
+  {
+    path: '/projects',
+    element: <div>Projects Page</div>,
   },
 ]);
 
